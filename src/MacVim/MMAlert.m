8b46ccf9b143b549b7546e6e72c1f41e91303d75
Tae Won Ha <qvacua@gmail.com>
2013-06-02 14:04:34 +0200

    correct header for alert.m

--- src/MacVim/MMAlert.m
+++ src/MacVim/MMAlert.m
@@ -1,9 +1,12 @@
-//
-// Created by Tae Won Ha on 5/30/13.
-//
-// To change the template use AppCode | Preferences | File Templates.
-//
-
+/* vi:set ts=8 sts=4 sw=4 ft=objc:
+ *
+ * VIM - Vi IMproved		by Bram Moolenaar
+ *				MacVim GUI port by Bjorn Winckler
+ *
+ * Do ":help uganda"  in Vim to read copying and usage conditions.
+ * Do ":help credits" in Vim to see a list of people who contributed.
+ * See README.txt for an overview of the Vim source code.
+ */
 
 #import "MMAlert.h"
 #import "MMLog.h"
